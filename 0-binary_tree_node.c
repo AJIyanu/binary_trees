f6218460fa9c2f4daa7257fa6319d32eab32457e
AJIyanu <aderemijoseph@live.com>
2022-08-25 18:53:28 +0100

    chrck

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,12 +11,21 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	parent = malloc(sizeof(binary_tree_t));
-	if (parent == NULL)
-		return (NULL);
-	parent->n = value;
-	parent->left = NULL;
-	parent->right = NULL;
+	binary_tree_t *ptr = NULL;
 
-	return (parent);
+	ptr = malloc(sizeof(binary_tree_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->n = value;
+	ptr->left = NULL;
+	ptr->right = NULL;
+
+	if (parent == NULL)
+		parent = ptr;
+	else if (parent->left == NULL)
+		parent->left = ptr;
+	else if (parent->right == NULL)
+		parent->right = NULL;
+
+	return (ptr);
 }
