cffa2d620086fcf83b3e219435728f839ff93370
AJIyanu <aderemijoseph@live.com>
2022-08-25 17:07:19 +0100

    ko ye mi ooooo

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * binary_tree_node - create a binary tree node
@@ -15,12 +14,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	parent = malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
 		return (NULL);
-	printf("malloc created succesful\n");
 	parent->n = value;
 	parent->left = NULL;
 	parent->right = NULL;
-    
-	printf("Node Created\n");
-	return (parent);
 
+	return (parent);
 }
