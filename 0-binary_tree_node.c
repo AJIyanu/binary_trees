5693b3fa6cbf4fc47ee598dd1fe00c0e2defbba6
AJIyanu <aderemijoseph@live.com>
2022-08-25 00:32:19 -0700

    what requirement

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,6 +14,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (parent == NULL)
 	{
 		parent = malloc(sizeof(binary_tree_t));
+		if (parent == NULL)
+			return (NULL);
 		parent->n = value;
 		parent->left = NULL;
 		parent->right = NULL;
