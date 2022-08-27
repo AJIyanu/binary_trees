037751f3af17bfdfb1d89514d272d1d05ca5391f
AJIyanu <aderemijoseph@live.com>
2022-08-27 21:26:03 +0100

    got me with;

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,7 +9,7 @@
  * Return: nothing just prints
  */
 
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	const binary_tree_t *ptr = tree;
 
