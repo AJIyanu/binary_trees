68623250f3d25e93dadb570e42c05da769ebd6a4
AJIyanu <aderemijoseph@live.com>
2022-08-27 01:48:46 +0100

    delete recursive

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,4 +10,17 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
+	binary_tree_t *ptr = tree;
 
+	if (ptr == NULL)
+		EXIT_SUCCESS;
+	if (ptr->left == NULL)
+	{
+		if (ptr->right == NULL)
+			free(ptr);
+		else
+			binary_tree_delete(ptr->right);
+	}
+	else
+		binary_tree_delete(ptr->left);
+}
