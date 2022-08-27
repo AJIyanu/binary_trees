1f263506b363afab9993d79107acf69d228db782
AJIyanu <aderemijoseph@live.com>
2022-08-27 02:31:13 +0100

    Handled NULL not NULl

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 	{
 		free(ptr);
-		return (NULl);
+		return (NULL);
 	}
 	if (parent->right == NULL)
 		parent->right = ptr;
