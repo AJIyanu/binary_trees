446b914f193c339ea745a1a6cf6cddf1c35e18a8
AJIyanu <aderemijoseph@live.com>
2022-08-27 01:37:20 +0100

    vscode

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes the entire tree
+ * @tree: pointer to the root of node
+ *
+ * Return: no shit
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+
